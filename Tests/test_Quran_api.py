6ed66669e6b191333bf3a1bbbd88ee4015ba1bcd
Alaa <masalha.alaa@gmail.com>
2025-05-31 09:57:29 +0300

    - Fixed get_tafsir_from_verse_id endpoint. - Replaced http with https in base URL. - Removed unnecessary port number in tests. - Added tests.

--- Tests/test_Quran_api.py
+++ Tests/test_Quran_api.py
@@ -28,55 +28,59 @@ qur = Quran()
 
 def test_get_recitations():
     assert qur.get_recitations() == rq.get(
-        "http://api.quran.com:3000/api/v3/options/recitations").json()
+        "https://api.quran.com/api/v3/options/recitations").json()
 
 def test_get_translations():
     assert qur.get_translations() == rq.get(
-        "http://api.quran.com:3000/api/v3/options/translations").json()
+        "https://api.quran.com/api/v3/options/translations").json()
 
 def test_get_languages():
     assert qur.get_languages(language='ur') == rq.get(
-        "http://api.quran.com:3000/api/v3/options/languages?language=bn").json()
+        "https://api.quran.com/api/v3/options/languages?language=bn").json()
 
 def test_get_tafsirs():
     assert qur.get_tafsirs() == rq.get(
-        "http://api.quran.com:3000/api/v3/options/tafsirs").json()
+        "https://api.quran.com/api/v3/options/tafsirs").json()
 
 def test_get_chapter():
     assert qur.get_chapter(1, language="en") == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1?language=en").json()
+        "https://api.quran.com/api/v3/chapters/1?language=en").json()
 
 def test_get_chapters():
     qur.get_chapter(language="en") # == rq.get(
-    #     "http://api.quran.com:3000/api/v3/chapters?language=en").json()
+    #     "https://api.quran.com/api/v3/chapters?language=en").json()
 
 def test_get_chapter_info():
     assert qur.get_chapter(1, info=True, language="en") == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1/info?language=en").json()
+        "https://api.quran.com/api/v3/chapters/1/info?language=en").json()
 
 def test_get_verses():
     assert qur.get_verses(1, text_type="words") == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1/verses?text_type=words").json()
+        "https://api.quran.com/api/v3/chapters/1/verses?text_type=words").json()
 
 def test_get_verse():
     assert qur.get_verse(chapter_id=1, verse_id=1) == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1/verses/1").json()
+        "https://api.quran.com/api/v3/chapters/1/verses/1").json()
 
 def test_get_juzs():
     assert qur.get_juzs() == rq.get(
-        "http://api.quran.com:3000/api/v3/juzs").json()
+        "https://api.quran.com/api/v3/juzs").json()
 
 def test_get_tafsirs_from_verse_id():
     assert qur.get_tafsirs_from_verse_id(chapter_id=1, verse_id=1) == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1/verses/1/tafsirs").json()
+        "https://api.quran.com/api/v3/chapters/1/verses/1/tafsirs").json()
 
-def test_get_tafsir_from_verse_id():
-    assert qur.get_tafsir_from_verse_id(chapter_id=1, verse_id=1, tafsirs="ar_baghawy") == rq.get(
-        "http://api.quran.com:3000/api/v3/chapters/1/verses/1/tafsirs?tafsirs=ar_baghawy").json()
+def test_get_tafsir_from_verse_id_and_tafsir_slug():
+    assert qur.get_tafsir_from_verse_id(chapter_id=1, verse_id=1, tafsir_id="ar-tafsir-al-baghawi") == rq.get(
+        "https://api.quran.com/api/v3/chapters/1/verses/1/tafsirs/ar-tafsir-al-baghawi").json()
+
+def test_get_tafsir_from_verse_id_and_tafsir_id():
+    assert qur.get_tafsir_from_verse_id(chapter_id=1, verse_id=1, tafsir_id=94) == rq.get(
+        "https://api.quran.com/api/v3/chapters/1/verses/1/tafsirs/94").json()
 
 def test_search():
     assert qur.search(q="imran", size=20, page=0, language="en") == rq.get(
-        "http://api.quran.com:3000/api/v3/search?q=imran&size=20&page=0&language=en").json()
+        "https://api.quran.com/api/v3/search?q=imran&size=20&page=0&language=en").json()
 
 
 print(test_get_chapters())
